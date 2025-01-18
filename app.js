b253349e6664fe72067a67a0876dd039de371e25
syakaizin <lemon62150@gmail.com>
2025-01-19 04:06:09 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let menu0Detail = document.querySelector("#menu0");
+let menu1Detail = document.querySelector("#menu1");
+let menu2Detail = document.querySelector("#menu2");
+let menu3Detail = document.querySelector("#menu3");
+let menu4Detail = document.querySelector("#menu4");
+let menu5Detail = document.querySelector("#menu5");
+let lastselect = 'option';
 let sumMoney = 0;
 let displaySumMoney = ""
 let displaySumMoneyCard = ""
@@ -11,11 +18,71 @@ let selectMenu = "";
 document.getElementById('recentlySelectMenu').textContent = recentlySelectMenu;
 const selectmenu = document.querySelector("#selectListMoney");
 selectmenu.classList.toggle("hidden");
+this.ngOnInit();
+
+function ngOnInit() {
+    // オプションのみ初期表示、あとはCSSで隠すようにする
+    menu1Detail.classList.add("hidden");
+    menu2Detail.classList.add("hidden");
+    menu3Detail.classList.add("hidden");
+    menu4Detail.classList.add("hidden");
+    menu5Detail.classList.add("hidden");
+
+}
 
 function changeButtonDisable(buttonStatus) {
     // ボタンを活性化
     const moneyButton = document.getElementById('moneyButton');
     moneyButton.disabled = buttonStatus;
+}
+
+function changeDiplayMenu(select) {
+    // 押したタブのものを表示
+    switch (select) {
+        case 'option':
+            menu0Detail.classList.remove("hidden");
+            break;
+        case 'set':
+            menu1Detail.classList.remove("hidden");
+            break;
+        case 'drink':
+            menu2Detail.classList.remove("hidden");
+            break;
+        case 'food':
+            menu3Detail.classList.remove("hidden");
+            break;
+        case 'pic':
+            menu4Detail.classList.remove("hidden");
+            break;
+        case 'other':
+            menu5Detail.classList.remove("hidden");
+            break;
+    }
+    // 表示してるものと違うものを表示させる場合
+    if (select !== lastselect) {
+        // 今表示してたものを隠す
+        switch (lastselect) {
+            case 'option':
+                menu0Detail.classList.add("hidden");
+                break;
+            case 'set':
+                menu1Detail.classList.add("hidden");
+                break;
+            case 'drink':
+                menu2Detail.classList.add("hidden");
+                break;
+            case 'food':
+                menu3Detail.classList.add("hidden");
+                break;
+            case 'pic':
+                menu4Detail.classList.add("hidden");
+                break;
+            case 'other':
+                menu5Detail.classList.add("hidden");
+                break;
+        }
+        lastselect = select;
+    }
 }
 
 function changeFlg() {
@@ -70,16 +137,21 @@ function sumDrinkMoney(idx) {
             selectMenu += recentlySelectMenu;
             break;
         case 4:
+            sumMoney += 3900;
+            recentlySelectMenu = "シャンメリー\n";
+            selectMenu += recentlySelectMenu;
+            break;
+        case 5:
             sumMoney += 10000;
             recentlySelectMenu = "カフェ・ド・パリ\n";
             selectMenu += recentlySelectMenu;
             break;
-        case 5:
+        case 6:
             sumMoney += 25000;
             recentlySelectMenu = "オリシャン\n";
             selectMenu += recentlySelectMenu;
             break;
-        case 6:
+        case 7:
             sumMoney += 20000;
             recentlySelectMenu = "マバム\n";
             selectMenu += recentlySelectMenu;
@@ -105,31 +177,38 @@ function sumFoodMoney(idx) {
     switch (idx) {
         case 1:
             sumMoney += 1300;
-            selectMenu += "君がオレンジを抱いて///\n";
+            recentlySelectMenu = "君がオレンジを抱いて///\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 2:
             sumMoney += 1100;
-            selectMenu += "ナポリの風\n";
+            recentlySelectMenu = "ナポリの風\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 3:
             sumMoney += 600;
-            selectMenu += "これは流石にポッキー!\n";
+            recentlySelectMenu = "これは流石にポッキー!\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 4:
             sumMoney += 700;
-            selectMenu += "渇きに飢えた者たちへの褒美\n";
+            recentlySelectMenu = "渇きに飢えた者たちへの褒美\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 5:
             sumMoney += 800;
-            selectMenu += "白と黒の社交ダンスT盛り\n";
+            recentlySelectMenu = "白と黒の社交ダンスT盛り\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 6:
             sumMoney += 600;
-            selectMenu += "脳の〆作業\n";
+            recentlySelectMenu = "脳の〆作業\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 10:
             sumMoney += 1000;
-            selectMenu += "もちこみ\n";
+            recentlySelectMenu = "もちこみ\n";
+            selectMenu += recentlySelectMenu;
             break;
 
         // フードもっとあるので追加予定
@@ -152,15 +231,18 @@ function sumPhotoMoney(idx) {
     switch (idx) {
         case 1:
             sumMoney += 1300;
-            selectMenu += "ソロチェキ\n";
+            recentlySelectMenu = "ソロチェキ\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 2:
             sumMoney += 1800;
-            selectMenu += "ツーチェキ\n";
+            recentlySelectMenu = "ツーチェキ\n";
+            selectMenu += recentlySelectMenu;
             break;
         case 3:
             sumMoney += 2600;
-            selectMenu += "フード付きチェキ\n";
+            recentlySelectMenu = "フード付きチェキ\n"
+            selectMenu += recentlySelectMenu;
             break;
     }
     this.displaySelectedMenu();
@@ -171,7 +253,8 @@ function count() {
     let textbox = document.getElementById('times');
     if (Number(textbox.value) >= 1) {
         sumMoney += 1000 * Number(textbox.value);
-        selectMenu += "くじ/チンチロ ×" + Number(textbox.value).toString() + "回\n";
+        recentlySelectMenu = "くじ/チンチロ ×" + Number(textbox.value).toString() + "回\n";
+        selectMenu += recentlySelectMenu;
         this.displaySelectedMenu();
         textbox.value = '';
         this.changeButtonDisable(false);
@@ -182,7 +265,8 @@ function count2() {
     let textbox2 = document.getElementById('times2');
     if (Number(textbox2.value) >= 1) {
         sumMoney += 1500 * Number(textbox2.value);
-        selectMenu += "てきーら ×" + Number(textbox2.value).toString() + "杯\n";
+        recentlySelectMenu = "てきーら ×" + Number(textbox2.value).toString() + "杯\n";
+        selectMenu += recentlySelectMenu;
         this.displaySelectedMenu();
         textbox2.value = '';
         this.changeButtonDisable(false);
@@ -193,7 +277,8 @@ function sumTextboxValue() {
     const value = document.getElementById('noMenu');
     if (Number(value.value) >= 100) {
         sumMoney += Number(value.value);
-        selectMenu += Number(value.value).toString() + "\n";
+        recentlySelectMenu = Number(value.value).toString() + "\n";
+        selectMenu += recentlySelectMenu;
         this.displaySelectedMenu();
         value.value = '';
         this.changeButtonDisable(false);
